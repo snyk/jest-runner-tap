35b9c6572e0ebca9c4fca4cec53798b1265cf131
Chris West (Faux) <git@goeswhere.com>
2020-03-20 12:30:36 +0000

    test: two

--- test/translate.spec.ts
+++ test/translate.spec.ts
@@ -3,7 +3,7 @@ import type { AssertionResult } from '@jest/test-result';
 import { makeParser, translateResult } from '../lib/translator';
 
 describe('translation', () => {
-  it('singular test', async () => {
+  it('handles a test with one assert', async () => {
     const result = await testTranslation('./fixtures/one.tap');
     expect(result.failureMessage).toBeUndefined();
     expect(result.numFailingTests).toBe(0);
@@ -15,6 +15,32 @@ describe('translation', () => {
         title: 'hello',
         status: 'passed',
         numPassingAsserts: 1,
+        failureMessages: [],
+        ancestorTitles: [],
+      } as Partial<AssertionResult>),
+    ]);
+    expect(result.testResults[0].duration).toBeGreaterThan(0);
+  });
+
+  it('handles two passing tests', async () => {
+    const result = await testTranslation('./fixtures/two.tap');
+    expect(result.failureMessage).toBeUndefined();
+    expect(result.numFailingTests).toBe(0);
+    expect(result.numPendingTests).toBe(0);
+    expect(result.numPassingTests).toBe(2);
+    expect(result.skipped).toBe(false);
+    expect(result.testResults).toStrictEqual([
+      expect.objectContaining({
+        title: 'alpha',
+        status: 'passed',
+        numPassingAsserts: 2,
+        failureMessages: [],
+        ancestorTitles: [],
+      } as Partial<AssertionResult>),
+      expect.objectContaining({
+        title: 'bravo',
+        status: 'passed',
+        numPassingAsserts: 2,
         failureMessages: [],
         ancestorTitles: [],
       } as Partial<AssertionResult>),
