badef310c4be70cbf668f5ef91f5b5c4c5608a25
Chris West (Faux) <git@goeswhere.com>
2020-03-20 11:08:42 +0000

    chore: renames

---
+++ lib/tree/builder.ts
@@ -0,0 +1,47 @@
+import TapParser = require('tap-parser');
+import type { Result } from 'tap-parser';
+import {inspect} from "util";
+
+interface ChildNode {
+  kind: 'child';
+  result: Result;
+  children: TreeNode[];
+}
+
+interface AssertNode {
+  kind: 'assert';
+  result: Result;
+}
+
+type TreeNode = ChildNode | AssertNode;
+
+export class BuildTree {
+  readonly ours: TreeNode[] = [];
+  private currentChild?: BuildTree;
+
+  constructor(parser: TapParser) {
+    parser.on('child', (child) => {
+      if (undefined !== this.currentChild) {
+        throw new Error(
+          `new child while we still had events: ${inspect(this.ours)}`,
+        );
+      }
+      this.currentChild = new BuildTree(child);
+    });
+    parser.on('assert', (result) => {
+      if (undefined !== this.currentChild) {
+        this.ours.push({
+          kind: 'child',
+          result,
+          children: [...this.currentChild.ours],
+        });
+        this.currentChild = undefined;
+      } else {
+        this.ours.push({
+          kind: 'assert',
+          result,
+        });
+      }
+    });
+  }
+}
--- test/manual.spec.ts
+++ test/manual.spec.ts
@@ -1,64 +1,20 @@
 import * as fs from 'fs-extra';
 import TapParser = require('tap-parser');
-import type { Result } from 'tap-parser';
 import { inspect } from 'util';
-
-interface ChildResult {
-  kind: 'child';
-  result: Result;
-  children: ResultyThing[];
-}
-
-interface AssertResult {
-  kind: 'assert';
-  result: Result;
-}
-
-type ResultyThing = ChildResult | AssertResult;
-
-class Grabber {
-  readonly ours: ResultyThing[] = [];
-  private currentChild?: Grabber;
-
-  constructor(parser: TapParser) {
-    parser.on('child', (child) => {
-      if (undefined !== this.currentChild) {
-        throw new Error(
-          'new child while we still had events: ' + inspect(this.ours),
-        );
-      }
-      this.currentChild = new Grabber(child);
-    });
-    parser.on('assert', (result) => {
-      if (undefined !== this.currentChild) {
-        this.ours.push({
-          kind: 'child',
-          result,
-          children: [...this.currentChild.ours],
-        });
-        this.currentChild = undefined;
-      } else {
-        this.ours.push({
-          kind: 'assert',
-          result,
-        });
-      }
-    });
-  }
-}
+import { BuildTree } from '../lib/tree/builder';
 
 describe('manual parser', () => {
   it('handles one', async () => {
     const parser = new TapParser();
-    const grabber = new Grabber(parser);
+    const builder = new BuildTree(parser);
     await feed('./fixtures/one.tap', parser);
-    console.dir(inspect(grabber.ours, false, 8));
+    console.dir(inspect(builder.ours, false, 8));
   });
   it('handles two', async () => {
     const parser = new TapParser();
-    const grabber = new Grabber(parser);
+    const builder = new BuildTree(parser);
     await feed('./fixtures/two.tap', parser);
-    console.dir(inspect(grabber.ours, false, 8));
+    console.dir(inspect(builder.ours, false, 8));
   });
 });
 
