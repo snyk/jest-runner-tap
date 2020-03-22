07eb8485c42c9361ab206c1654227a5fbb6b2551
Chris West (Faux) <git@goeswhere.com>
2020-03-22 14:02:40 +0000

    chore: rename fixtures

--- test/fixtures/jest-demo.config.js
+++ test/fixtures/jest-demo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   projects: [
     {
       testRunner: '../..',
-      testMatch: ['<rootDir>/**/*.js'],
+      testMatch: ['<rootDir>/**/*.test.js'],
       globals: {
         'jest-runner-tap': {
           tapCommand: ['../../node_modules/.bin/tap', '-R', 'tap'],
